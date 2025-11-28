44c728696ad1481f95775630bdec8cd686376f38
baka9131 <baka9131@gmail.com>
2025-11-28 23:48:27 +0900

    feat: Feature-First 구조로 GNB 및 화면 구성
    - 5개 탭 구현 (홈, 지도, 스탬프, 보관함, My)
    - Feature-First 폴더 구조 적용
    - 커스텀 BottomNavBar 위젯 구현
    - 스탬프 아이콘 특별 스타일링 적용

---
+++ lib/features/storage/view/storage_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StorageView extends StatelessWidget {
+  const StorageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('보관함'),
+      ),
+    );
+  }
+}
