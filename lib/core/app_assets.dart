3f471910dd19ce23221bfa8ed206ff53e219fa6b
baka9131 <baka9131@gmail.com>
2025-11-28 23:47:39 +0900

    feat: 디자인 시스템 및 공통 유틸리티 구성
    - 컬러 팔레트 정의 (Primary, Sub, Gray, Text)
    - 에셋 경로 관리 클래스 추가
    - BuildContext extension으로 l10n 접근 간소화
    - Pretendard 폰트 파일 추가
    - GNB용 SVG 아이콘 추가

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  AppAssets._();
+
+  // SVG Icons
+  static const String iconHome = 'assets/svg/home.svg';
+  static const String iconLocation = 'assets/svg/location.svg';
+  static const String iconStamp = 'assets/svg/stamp.svg';
+  static const String iconSave = 'assets/svg/save.svg';
+  static const String iconMy = 'assets/svg/my.svg';
+}
