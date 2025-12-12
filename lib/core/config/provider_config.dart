b5edf191952ea617906a618cf2ff17e81034b665
hodu-angel <kinnylovemy@gmail.com>
2025-12-13 01:28:27 +0900

    refactor: MultiProvider providers를 메서드 전달방식에서 final로 변경

--- lib/core/config/provider_config.dart
+++ lib/core/config/provider_config.dart
@@ -5,11 +5,8 @@ import 'package:arttrip/features/home/home_repository_mock.dart';
 import 'package:arttrip/features/home/home_viewmodel.dart';
 import 'package:provider/provider.dart';
 
-List<ChangeNotifierProvider> getProviders() {
-  return [
-    ChangeNotifierProvider<HomeViewModel>(
-      create: (_) =>
-          HomeViewModel(AppConsts.useMock ? HomeRepositoryMockImpl() : HomeRepositoryImpl(DioClient.instance)),
-    ),
-  ];
-}
+final List<ChangeNotifierProvider> getProviders = [
+  ChangeNotifierProvider<HomeViewModel>(
+    create: (_) => HomeViewModel(AppConsts.useMock ? HomeRepositoryMockImpl() : HomeRepositoryImpl(DioClient.instance)),
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ void main() async {
   );
 
   runApp(MultiProvider(
-    providers: getProviders(),
+    providers: getProviders,
     child: const ArtTripApp(),
   ));
 }
