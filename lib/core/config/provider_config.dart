75d6416a037dc203312ba196ba0f80f758be8afa
baka9131 <baka9131@gmail.com>
2025-12-14 10:48:58 +0900

    feat: 온보딩 키워드 페이지 라우팅 및 Provider 설정
    - KeywordsViewModel Provider 추가
    - /onboarding/keywords 라우트 등록

--- lib/core/config/provider_config.dart
+++ lib/core/config/provider_config.dart
@@ -3,10 +3,16 @@ import 'package:arttrip/core/network/network.dart';
 import 'package:arttrip/features/home/home_repository.dart';
 import 'package:arttrip/features/home/home_repository_mock.dart';
 import 'package:arttrip/features/home/home_viewmodel.dart';
+import 'package:arttrip/features/onboarding/data/keywords_repository.dart';
+import 'package:arttrip/features/onboarding/data/keywords_repository_mock.dart';
+import 'package:arttrip/features/onboarding/viewmodel/keywords_viewmodel.dart';
 import 'package:provider/provider.dart';
 
 final List<ChangeNotifierProvider> getProviders = [
   ChangeNotifierProvider<HomeViewModel>(
     create: (_) => HomeViewModel(AppConsts.useMock ? HomeRepositoryMockImpl() : HomeRepositoryImpl(DioClient.instance)),
+  ),
+  ChangeNotifierProvider<KeywordsViewModel>(
+    create: (_) => KeywordsViewModel(AppConsts.useMock ? KeywordsRepositoryMockImpl() : KeywordsRepositoryImpl(DioClient.instance)),
   ),
 ];
