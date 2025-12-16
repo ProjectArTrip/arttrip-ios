0894af5d175e3eba6c4dbb40812a90d09d676c56
Ju <kinnylovemy@gmail.com>
2025-12-17 01:56:50 +0900

    feat: homepage 초기 데이터 로드 추가

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:arttrip/core/app_assets.dart';
 import 'package:arttrip/core/app_colors.dart';
+import 'package:arttrip/features/home/home_viewmodel.dart';
 import 'package:arttrip/features/home/views/international_domestic_tab_view.dart';
 import 'package:arttrip/features/home/views/today_exhibit_recommendation_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,6 +17,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      var homeViewModel = Provider.of<HomeViewModel>(context, listen: false);
+      homeViewModel.load(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
