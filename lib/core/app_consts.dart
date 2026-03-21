cc24df1ec950fb3d4de6e609713a908e56f79430
현규 <baka9131@gmail.com>
2026-03-21 19:57:46 +0900

    chore: useMock 비활성화 및 pubspec.lock 갱신

--- lib/core/app_consts.dart
+++ lib/core/app_consts.dart
@@ -1,7 +1,7 @@
 class AppConsts {
   const AppConsts._();
 
-  static const bool useMock = true; // Force using dummy data
+  static const bool useMock = false; // Force using dummy data
   static const int mockLoadingDelayMs =
       500; // Dummy data loading delay in milliseconds
 
