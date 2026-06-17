9f806d7bbfbaeb8e3f83ea0e2ea61780da45736e
Ju <kinnylovemy@gmail.com>
2026-06-17 12:50:47 +0900

    fix: fetch큐레이션 데이터 변경시 nofiy없던 부분 추가

--- lib/features/home/views/curation_view.dart
+++ lib/features/home/views/curation_view.dart
@@ -39,7 +39,7 @@ class _CurationViewState extends State<CurationView> {
             return Selector<HomeViewModel, AsyncState<CurationModel>>(
               selector: (_, vm) =>
                   vm.curations[vm.locationType]?[vm.area[vm.locationType]!] ??
-                  const AsyncState.error(),
+                  const AsyncState.loading(),
               builder: (context, state, _) {
                 return AsyncView(
                   state: state,
@@ -111,7 +111,9 @@ class _CurationViewState extends State<CurationView> {
                     );
                   },
                   onLoading: () => _buildCurationLoadingView(),
-                  onError: ({error}) => _buildEmptyView('', '', '0'),
+                  onError: ({error}) {
+                    return _buildEmptyView('', '', '0');
+                  },
                 );
               },
             );
